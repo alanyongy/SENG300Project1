ad46740c9af93b396efc2d370884914ff32f45db
jaden.taylor <jaden.taylor@ucalgary.ca>
2023-10-31 20:17:54 -0600

    Added initial CustomerStationControl class

---
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/CustomerStationControl.java
@@ -0,0 +1,11 @@
+// Class that contains actions that can be performed by a customer station
+// Authors:
+// Jaden Taylor (30113034)
+// TODO: Add your names and if you are the last person to do so delete this comment
+package com.thelocalmarketplace.software;
+
+import com.thelocalmarketplace.hardware.*;
+
+public class CustomerStationControl {
+
+}
