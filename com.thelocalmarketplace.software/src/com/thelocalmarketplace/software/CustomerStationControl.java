09617345eadb7c29cb7b4a20cd3a13675f13a322
ananditamahika <84046117+ananditamahika@users.noreply.github.com>
2023-11-02 23:06:36 -0600

    added empty classes for weight and add, pay and Order are implemented

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/CustomerStationControl.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/CustomerStationControl.java
@@ -5,7 +5,56 @@
 package com.thelocalmarketplace.software;
 
 import com.thelocalmarketplace.hardware.*;
+import com.tdc.*;
+import com.tdc.coin.*;
 
 public class CustomerStationControl {
-
+	
+	private SelfCheckoutStation station;
+	
+	private PayCoin payCoinController;
+	private Order order;
+	
+	public CustomerStationControl(SelfCheckoutStation customerStationControl) {
+		this.station = customerStationControl;
+		
+		//have to initialize weight dicrepency and add item controllers too (same way)
+		
+		
+		//initialize PayCoin control
+		payCoinController = new PayCoin(this);
+		customerStationControl.coinValidator.attach(payCoinController);
+		customerStationControl.coinStorage.attach(payCoinController);
+	}
+	
+	public SelfCheckoutStation getSelfCheckoutStation() {
+		return station;
+	}
+	
+	public void startSession() {
+		order = new Order(this);
+	}
+	
+	public void pay() {
+		payCoinController.pay(order);
+	}
+	
+	//displays message to attendant
+	public void notifyAttendant(String message) {
+		System.out.println("Attendant: " + message);
+	}
+	
+	//displays message to customer
+	public void notifyCustomer(String message) {
+		System.out.println("Customer: " + message);
+	}
+	
+	public Order getOrder() {
+		return order;
+	}
+	
+	/**need methods
+	 * method that calls notifyCustomer to scan next item
+	 * method that adds BarcodedProduct to order -  probably calls add method from order 
+	 */
 }
