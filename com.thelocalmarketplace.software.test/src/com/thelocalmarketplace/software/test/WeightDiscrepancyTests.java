3e2bf3f07a117266bc439be3da7e5b09522e1761
alanyongy <alan.yong@ucalgary.ca>
2023-11-04 01:22:14 -0600

    finished(?) weightDiscrepancy tests

--- com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/WeightDiscrepancyTests.java
+++ com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/WeightDiscrepancyTests.java
@@ -2,13 +2,12 @@ package com.thelocalmarketplace.software.test;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.jjjwelectronics.Mass;
-import com.jjjwelectronics.OverloadedDevice;
+import com.jjjwelectronics.Item;
+import com.jjjwelectronics.scanner.BarcodedItem;
 import com.thelocalmarketplace.hardware.SelfCheckoutStation;
 import com.thelocalmarketplace.software.CustomerStationControl;
-import com.thelocalmarketplace.software.SessionItem;
 
-import java.math.BigDecimal;
+import ca.ucalgary.seng300.simulation.SimulationException;
 
 import org.junit.Assert;
 import powerutility.PowerGrid;
@@ -32,50 +31,97 @@ public class WeightDiscrepancyTests {
 		// Initialize database
 		ExampleItems.updateDatabase();
 	}
+	
 	/**
-	 * Creates a weight discrepancy and ensures that the attendant is notified. Currently uses
+	 * After fixing a discrepancy, the station is unblocked.
+	 */
+	@Test
+	public void correctDiscrepancy() {
+		createDiscrepancyByRemoving(ExampleItems.PotatoChips.barcodedItem);
+		station.baggingArea.addAnItem(ExampleItems.PotatoChips.barcodedItem);
+		Assert.assertTrue(!control.isBlocked());
+	}
+	
+	/**
+	 * The station is blocked after adding the wrong item to the bagging area.
+	 */
+	@Test
+	public void addWrongItem() {
+		station.scanner.scan(ExampleItems.PotatoChips.barcodedItem);
+		station.baggingArea.addAnItem(ExampleItems.AppleJuice.barcodedItem);
+		Assert.assertTrue(control.isBlocked());
+	}
+	
+	/**
+	 * The station is blocked after removing the correctly added item from the bagging area.
+	 */
+	@Test
+	public void removeCorrectlyAddedItem() {
+		station.scanner.scan(ExampleItems.PotatoChips.barcodedItem);
+		station.baggingArea.addAnItem(ExampleItems.PotatoChips.barcodedItem);
+		station.baggingArea.removeAnItem(ExampleItems.PotatoChips.barcodedItem);
+		Assert.assertTrue(control.isBlocked());
+	}
+	
+	/**
+	 * The station is blocked after adding an additional item after correctly adding an item to the bagging area.
+	 */
+	@Test
+	public void addAdditionalItem() {
+		station.scanner.scan(ExampleItems.PotatoChips.barcodedItem);
+		station.baggingArea.addAnItem(ExampleItems.PotatoChips.barcodedItem);
+		station.baggingArea.addAnItem(ExampleItems.AppleJuice.barcodedItem);
+		Assert.assertTrue(control.isBlocked());
+	}
+	
+	/**
+	 * The attendant is notified after a weight discrepancy occurs. Currently uses
 	 * a variable in control for testing due to lack signaling UI implementation.
 	 */
 	@Test
 	public void discrepancyAttendantNotified() {
-		createDiscrepancy();
+		createDiscrepancyByRemoving(ExampleItems.PotatoChips.barcodedItem);
 		Assert.assertEquals(control.attendantNotified, control.notifyDiscrepancyCode);
 	}
 	
 	/**
-	 * Creates a weight discrepancy and ensures that the customer is notified. Currently uses
+	 * The customer is notified after a weight discrepancy occurs. Currently uses
 	 * a variable in control for testing due to lack signaling UI implementation.
 	 */
 	@Test
 	public void discrepancyCustomerNotified() {
-		createDiscrepancy();
+		createDiscrepancyByRemoving(ExampleItems.PotatoChips.barcodedItem);
 		Assert.assertEquals(control.customerNotified, control.notifyDiscrepancyCode);
 	}
 	
 	/**
-	 * Creates a weight discrepancy and ensures that the station is unable to add items.
+	 * The station is unable to add items to the order after a discrepancy occurs.
 	 */
 	@Test
 	public void discrepancyAddItem() {
-		createDiscrepancy();
+		createDiscrepancyByRemoving(ExampleItems.PotatoChips.barcodedItem);
 		station.scanner.scan(ExampleItems.AppleJuice.barcodedItem);
 		Assert.assertTrue(control.getOrder().getItems().size()==1);
 	}
 	
 	/**
-	 * Creates a weight discrepancy and ensures that the station is unable to initiate payment, 
-	 * thus not prompting the customer to insert payment.
+	 * The station is unable to pay for the order after a discrepancy occurs.
+	 * This condition is checked by the lack of prompts to the customer to insert payment 
+	 * after attempting to start the payment process.
 	 */
 	@Test
 	public void discrepancyPay() {
-		createDiscrepancy();
+		createDiscrepancyByRemoving(ExampleItems.PotatoChips.barcodedItem);
 		control.pay();
 		Assert.assertFalse(control.customerNotified == control.notifyInsertPaymentCode);
 	}
 	
-	private void createDiscrepancy() {
-		station.scanner.scan(ExampleItems.PotatoChips.barcodedItem);
-		station.baggingArea.addAnItem(ExampleItems.PotatoChips.barcodedItem);
-		station.baggingArea.removeAnItem(ExampleItems.PotatoChips.barcodedItem);
+	/**
+	 * Creates a weight discrepancy by removing a correctly added item from the bagging area.
+	 */
+	private void createDiscrepancyByRemoving(BarcodedItem item) {
+		station.scanner.scan(item);
+		station.baggingArea.addAnItem(item);
+		station.baggingArea.removeAnItem(item);
 	}
 }
