e6e4d47b40030d9ea04dba4ba308fec18f2d0a4d
ananditamahika <84046117+ananditamahika@users.noreply.github.com>
2023-11-04 16:43:56 -0600

    commenting updated

--- com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayCoin.java
+++ com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/PayCoin.java
@@ -12,8 +12,25 @@ import com.tdc.coin.CoinStorageUnitObserver;
 import com.tdc.coin.CoinValidator;
 import com.tdc.coin.CoinValidatorObserver;
 
+/**
+ * PayCoin controls all coin payments. PayCoin object is instantiated by CustomerStationControl class, 
+ * where it waits for a valid coin to be detected by CoinValidatorObserver, which is then processed as 
+ * payment for the customer's order. The coin payment is then processed, and added to storage by 
+ * CoinStorageUnitObserver, which indicates successful order payment. 
+ * 
+ * <p>
+ * Implements {@link com.tdc.coin.CoinValidatorObserver CoinValidObserver},
+ * and {@link com.tdc.coin.CoinStorageUnitObserver}
+ * </p>
+ * 
+ */
+
 public class PayCoin extends AbstractPay implements CoinValidatorObserver, CoinStorageUnitObserver, CoinSlotObserver {
 
+	/**
+	 * creates PayCoin controller in CustomerStationControl class
+	 * @param customerStationControl the station to which the controller is implemented
+	 */
 	public PayCoin(CustomerStationControl customerStationControl) {
 		super(customerStationControl);
 	}
@@ -38,10 +55,16 @@ public class PayCoin extends AbstractPay implements CoinValidatorObserver, CoinS
 
 	@Override
 	public void turnedOff(IComponent<? extends IComponentObserver> component) {
-		// TODO Auto-generated method stub
+		// not used in this iteration
 		
 	}
 
+	/**
+	 * called when there is no empty space in coin storage. This means inserted coins cannot be processed 
+	 * for payment and notifies the attendant that the storage is full
+	 * 
+	 * @param unit CoinStorageUnit device that stores coins for the station
+	 */
 	@Override
 	public void coinsFull(CoinStorageUnit unit) {
 		coinAdded(unit);
@@ -49,6 +72,12 @@ public class PayCoin extends AbstractPay implements CoinValidatorObserver, CoinS
 
 	}
 
+	/**
+	 * Called when valid coins inserted for payment end up in storage (if there is space in storage),
+	 * which then can record the amount due/paid for the order. 
+	 * 
+	 * @param unit CoinStorageUnit device that stores coins for the station
+	 */
 	@Override
 	public void coinAdded(CoinStorageUnit unit) {
 		Order order = customerStationControl.getOrder();
@@ -68,18 +97,34 @@ public class PayCoin extends AbstractPay implements CoinValidatorObserver, CoinS
 		
 	}
 
+	/**
+	 * Called when valid coin is inserted into coin slot of station and starts the payment process
+	 * 
+	 * @param validator CoinValidator device that detects valid coins
+	 * @param value value of the coins inserted for payment
+	 */
 	@Override
 	public void validCoinDetected(CoinValidator validator, BigDecimal value) {
 		processPayment(validator, value);
 		
 	}
 
+	/**
+	 * Called when invalid coins (not supported for payment) are inserted into coin slot, which cannot 
+	 * start the payment process, so customer is notified to insert valid coins.
+	 * 
+	 * @param validator CoinValidator device that can detect invalid coins
+	 */
 	@Override
 	public void invalidCoinDetected(CoinValidator validator) {
 		customerStationControl.notifyCustomer("Please insert a valid coin", customerStationControl.notifyOtherCode);
 		
 	}
 
+	/**
+	 * Begins payment process by updating the remaining balance for the order if there is no weight discrepancy 
+	 * in the bagging area. Also sends the customer a message about how much is still due. 
+	 */
 	@Override
 	public BigDecimal pay(Order order) {
 		BigDecimal totalUnpaid = order.getTotalUnpaid();
@@ -91,12 +136,20 @@ public class PayCoin extends AbstractPay implements CoinValidatorObserver, CoinS
 	    return totalUnpaid;
 	}
 	
+	/**
+	 * notifies the customer of the unpaid amount for the order
+	 * @param order	the customer's order for which they are paying
+	 */
 	private void updateRemainingBalance(Order order) {
 		BigDecimal totalUnpaid = order.getTotalUnpaid();
 		customerStationControl = order.getCustomerStationControl();
 		customerStationControl.notifyCustomer(String.format("Amount due: %.2f", totalUnpaid), customerStationControl.notifyOtherCode);
 	}
 	
+	/**
+	 * Processes amountDue and notifies the customer of the amount still unpaid for the order
+	 * @param order the customer's order for which payment is being processed
+	 */
 	public void updatePayment(Order order) {
 		order.addCoinsPaid(amountDue);
 		amountDue = BigDecimal.ZERO;
@@ -118,11 +171,22 @@ public class PayCoin extends AbstractPay implements CoinValidatorObserver, CoinS
 	}
 		
 
+	/**
+	 * After detecting the inserted coins, the updating payment process will be called to set the 
+	 * amountDue for the order. The paid amount is then subtracted from the order total when the 
+	 * coin reaches storage and calls coinsFull/coinsAdded.
+	 * 
+	 * @param validator CoinValidator device that validates coins for payment
+	 * @param num value of the coins inserted by customer for payment
+	*/
 	@Override
 	public void processPayment(CoinValidator validator, BigDecimal num) {
 		amountDue = num;
 	}
 
+	/*
+	 * Called once the total order has been paid for and sets amountDue back to 0
+	 */
 	@Override
 	public void fullPaid() {
 		amountDue = BigDecimal.ZERO;
@@ -132,12 +196,16 @@ public class PayCoin extends AbstractPay implements CoinValidatorObserver, CoinS
 
 	@Override
 	public void coinInserted(CoinSlot slot) {
-		// TODO Auto-generated method stub
+		// not used in this iteration
 		
 	}
 	
+	/**
+	 * 
+	 * @return amount due for the order
+	 */
 	public BigDecimal getAmountDue() {
-		return amountDue;
+		return amountDue; 
 	}
 	
 	
